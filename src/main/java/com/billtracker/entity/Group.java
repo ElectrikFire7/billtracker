1a5d203945329baf7f428bbd5d021f3c7a71740b
ElectrikFire7 <9kishlay@gmail.com>
2026-02-24 20:36:25 +0530

    fix: removing orphan values

--- src/main/java/com/billtracker/entity/Group.java
+++ src/main/java/com/billtracker/entity/Group.java
@@ -29,4 +29,16 @@ public class Group {
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private java.util.List<GroupMember> members = new java.util.ArrayList<>();
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private java.util.List<Bill> bills = new java.util.ArrayList<>();
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private java.util.List<Payment> payments = new java.util.ArrayList<>();
 }
